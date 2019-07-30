6fecb2646b8570487a46f637a85ef9d38cf7a8ea
Joshua-McGee <jcooler.cooler@gmail.com>
2019-07-30 21:32:52 +0000

    typewriter that prints a letter of a string with a 50 milisecond delay

--- typewriter.js
+++ typewriter.js
@@ -1,14 +1,15 @@
 const sentence = "hello there from lighthouse labs";
 
-setTimeout(() => {
-  for (let char of sentence) {
-    process.stdout.write(char) + '\n';
-  }
-}, 50);
 
-// for (let char of sentence) { // loops through each character in the string
-//   // console.log(char); console.log adds a new line character (/n) to the end of each string (character in this case)
-//   setTimeout(() => {
-//     process.stdout.write(char); // prints on the same line but so does our prompt.
-//   }, 1000);
-// }
+// Instead of the characters all appearing at the same time later on,
+// we need them each be staggered. Perhaps each one can be 50ms apart.
+
+//need a function that prints a character each time setTimeout is run.
+let time = 50;
+
+for (let char of sentence) {
+  setTimeout(function () {
+    console.log(char);
+  }, time);
+  time += 50;
+};
